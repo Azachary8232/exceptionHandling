dccd68cd1acfbb4d7f8e533c97d886ada7ea0795
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-11-29 20:29:48 -0700

    tried Running Code

--- ExceptionsTest.java
+++ ExceptionsTest.java
@@ -1,7 +1,7 @@
-
+import java.util.ArrayList;
 
 public class ExceptionsTest {
-    public static void main(String[], args) {
+    public static void main(String[] args) {
 
         ArrayList<Object> myList = new ArrayList<Object>();
         myList.add("13");
