6286b2a6b539300b2f9db833d16fa7a918ea3a43
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-11-29 20:46:49 -0700

    Changed

--- ExceptionsTest.java
+++ ExceptionsTest.java
@@ -1,7 +1,24 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class ExceptionsTest {
     public static void main(String[] args) {
+
+
+
+    class Broke extends Exception{}
+    class UnreliableFriend {
+        public boolean deliverMessage() throws OutOfGasException{
+            Random r = new Random();
+            boolean hasGas = r.nextBoolean();
+            if (hasGas){
+                return true;
+            }
+            throw new OutOfGasException();
+    }
+}
+
+
 
         ArrayList<Object> myList = new ArrayList<Object>();
         myList.add("13");
@@ -10,7 +27,13 @@ public class ExceptionsTest {
         myList.add("Goodbye World");
 
         for(int i = 0; i < myList.size(); i++) {
-            Integer castedValue = (Integer) myList.get(i);
+            try{
+                Integer castedValue = (Integer) myList.get(i);
+                System.out.println("Good");
+            }
+            catch (Broke e){
+                System.out.println("Works");
+            }
         }
 
     }
